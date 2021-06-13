8251d11869d242c9ebfc2ced65b263b899d0e5f8
desperato-search <ceciliaw326@gmail.com>
2021-06-13 13:49:41 +0200

    added forms

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,15 @@
 function startSearch () {
-    document.getElementById('textAreaChats').disabled = true;
-    document.getElementById('search-button').disabled = true;
-    document.getElementById('search-loading').removeAttribute("hidden");
-    document.getElementById('search-text-label-ru').innerText = "";
+    if (!checkTextAreaEmpty()) {
+        document.getElementById('textAreaChats').disabled = true;
+        document.getElementById('search-button').disabled = true;
+        document.getElementById('search-loading').removeAttribute("hidden");
+        document.getElementById('search-text-label-ru').innerText = "";
+    }
+}
+
+function checkTextAreaEmpty () {
+    if(document.getElementById("textAreaChats").value.replace(/\s+/g, ' ').trim() == '') {
+        return true
+    }
+    return false;
 }
