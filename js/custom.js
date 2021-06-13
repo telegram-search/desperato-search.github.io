01fca5490f0e29aaf7eaff9a882ae8f08f70046f
desperato-search <ceciliaw326@gmail.com>
2021-06-13 08:37:53 +0200

    minors

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+function startSearch () {
+    document.getElementById('textAreaChats').disabled = true;
+    document.getElementById('search-button').value = "1"
+    document.getElementById('search-button').disabled = false;
+}
+
+document.getElementById('search-button').onclick = function() {
+    // alert("button was clicked");
+};
