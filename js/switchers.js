620ae1237de942eb32eb5cbb039ae2e53fd313e0
desperato-search <ceciliaw326@gmail.com>
2021-06-18 18:22:05 +0200

    fixed js logic

--- js/switchers.js
+++ js/switchers.js
@@ -1,26 +1,13 @@
 function categorySearch(chatsCategory) {
     disableConfig()
 
-    var i;
-    var categories = document.getElementsByClassName('categories-list')[0].children;
-
-    var selectedStatusesArr = [];
-
-    for(i = 0; i < categories.length; i++) {
-        if(categories[i].nodeName === 'DIV') {
-            if(categories[i].childNodes[3].childNodes[1].value === '1') {
-                selectedStatusesArr.push('added');
-            }
-        }
-    }
-
     var checkboxState = document.getElementById(chatsCategory).value
 
     if(checkboxState === '0') {
         document.getElementById(chatsCategory).value = '1'
         document.getElementById(chatsCategory).checked = true
         document.getElementById('textAreaChats').disabled = false;
-        if(selectedStatusesArr.length === 0) {
+        if(getActiveOptions() === 0) {
             document.getElementById('textAreaChats').disabled = false;
         } else {
             document.getElementById('textAreaChats').disabled = true;
@@ -30,11 +17,10 @@ function categorySearch(chatsCategory) {
     if(checkboxState === '1') {
         document.getElementById(chatsCategory).value = '0'
         document.getElementById(chatsCategory).checked = false
-        document.getElementById('textAreaChats').disabled = true;
-        if(selectedStatusesArr.length === 0) {
-            document.getElementById('textAreaChats').disabled = true;
-        } else {
+        if(getActiveOptions() === 0) {
             document.getElementById('textAreaChats').disabled = false;
+        } else {
+            document.getElementById('textAreaChats').disabled = true;
         }
     }
 }
@@ -44,4 +30,21 @@ function disableConfig() {
     document.getElementById('ruChats').disabled = true;
     document.getElementById('enChats').disabled = true;
     document.getElementById('directSearch').disabled = true;
+}
+
+function getActiveOptions() {
+
+    var i;
+    var selectedStatusesArr = [];
+    var categories = document.getElementsByClassName('categories-list')[0].children;
+
+    for(i = 0; i < categories.length; i++) {
+        if(categories[i].nodeName === 'DIV') {
+            if(categories[i].childNodes[3].childNodes[1].value === '1') {
+                selectedStatusesArr.push('added');
+            }
+        }
+    }
+
+    return selectedStatusesArr.length;
 }
